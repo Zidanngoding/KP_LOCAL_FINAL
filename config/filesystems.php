13f20945b29606d8d003ec9c4ae0d06a5e9044c8
Zidanngoding <muhammadzidandako@gmail.com>
2026-01-05 01:06:29 +0700

    filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -49,12 +49,12 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
+            'key' => env('384170a8e0602c18240a4272a2cb300e'),
+            'secret' => env('sb_publishable_zOO3FkvrVR1qn834zmwtIw_HrHaj0jH'),
+            'region' => env('us-east-1'),
+            'bucket' => env('uploads'),
+            'url' => env('https://konxgvxqesfsijnnyaiu.supabase.co'),
+            'endpoint' => env('https://konxgvxqesfsijnnyaiu.supabase.co/storage/v1/object/public/uploads'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
             'report' => false,
